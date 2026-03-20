199ff3e0bf545a37e42aadd8db5cde4a8f7c8e88
NhungNhung02 <nhunglehong06022005@gmail.com>
2026-03-20 19:17:14 +0700

    BaiTap4

--- ThuThucHanhTuan5_fetch_api/src/App.jsx
+++ ThuThucHanhTuan5_fetch_api/src/App.jsx
@@ -1,121 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function FetchSearchFilter(props) {
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    try {
+      const url = "https://jsonplaceholder.typicode.com/posts"
+      async function fetchDataFromUrl(url) {
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data);
+        setFilteredData(data);
+        console.log("setFilteredData Fetch");
+      }
+      fetchDataFromUrl(url)
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (searchValue) {
+      const newData = data.filter(item => item.title.includes(searchValue))
+      // console.log(newData);
+      setFilteredData(newData)
+    } else {
+      if (data.length > 0) {
+        setFilteredData(data);
+      }
+    }
+  }, [searchValue])
+
+
+  function handleChange(e) {
+    const val = e.target.value;
+    setSearchValue(val)
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <input type="text" placeholder='Enter your search..' onChange={handleChange} value={searchValue} />
+      <ul>
+        {
+          filteredData.map(item => {
+            return <li key={item.id}>{item.title}</li>
+          })
+        }
+      </ul>
     </>
   )
 }
 
-export default App
+export default FetchSearchFilter;
